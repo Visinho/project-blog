9a90a2fb5a04b733608ebb3edcf286a242271998
Visinho <Elvisvisinho@rocketmail.com>
2022-09-09 11:18:10 +0100

    Routing of pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-// import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
-// import Write from "./pages/write/Write";
-// import Single from "./pages/single/Single"
+import Single from "./pages/single/Single";
 
 function App() {
   return (
-    <>
+  <Router>
    <TopBar/>
-   {/* <Home/> */}
-   {/* <Single/> */}
-   {/* <Write/> */}
-   <Settings />
-   </>
+   <Switch>
+    <Route exact path="/">
+      <Home/>
+    </Route>
+    <Route path="/register">
+      <Register/>
+    </Route>
+    <Route path="/login">
+      <Login/>
+    </Route>
+    <Route path="/write">
+      <Write/>
+    </Route>
+    <Route path="/settings">
+      <Settings/>
+    </Route>
+    <Route path="/post/:postId">
+      <Single/>
+    </Route>
+   </Switch>
+  </Router>
   );
 }
 
